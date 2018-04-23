ed550069e9b43914b2a7a83ace7f7c8b391105d6
AugmentedFourth <zcomito@gmail.com>
2018-04-23 01:06:25 -0700

    clap+fail

--- src/ast.rs
+++ src/ast.rs
@@ -1,27 +1,32 @@
 use rug::Integer;
 
 
+#[derive(Debug)]
 pub struct Prog {
     pub decls: Vec<Decl>,
 }
 
+#[derive(Debug)]
 pub struct Decl {
     pub name:   String,
     pub typing: Typing,
     pub body:   Expr,
 }
 
+#[derive(Debug)]
 pub enum Typing {
     Concrete(String),
     Function(Vec<Typing>, Vec<Typing>),
 }
 
+#[derive(Debug)]
 pub enum Expr {
     Literal(Literal),
     Id(String),
     Block(Vec<Expr>),
 }
 
+#[derive(Debug)]
 pub enum Literal {
     Int(Integer),
     Float(f64),
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,66 @@
+#[macro_use]
+extern crate clap;
+#[macro_use]
+extern crate failure;
 extern crate rug;
 
 mod ast;
 mod ataraxia;
 
+use ataraxia::ProgParser;
+use clap::{App, Arg};
+use failure::Error;
+use std::{fs::File, io::{BufReader, prelude::*}};
+
+
+fn main_() -> Result<(), Error> {
+    let m = App::new("ataraxiac")
+        .author("Zach Comito <zcomito@gmail.com>")
+        .about("A compiler for the ataraxia language.")
+        .long_about("A compiler for the ataraxia language.\n\n\
+
+                     ataraxiac emits bytecode that can then be interpreted, \
+                     or even used to generate native code through an \
+                     LLVM backend.\n\n\
+
+                     ataraxia is a high-level, statically typed, \
+                     stack-oriented programming language: similar to Joy, \
+                     Forth, or PostScript, but more similar to Rust in terms \
+                     of typechecking and handling of data.")
+        .version(crate_version!())
+        .arg(Arg::with_name("infile")
+             .index(1)
+             .required(true))
+        .arg(Arg::with_name("outfile")
+             .short("o")
+             .long("outfile")
+             .alias("out")
+             .takes_value(true))
+        .arg(Arg::with_name("syntax-only")
+             .short("s")
+             .long("syntax-only"))
+        .get_matches();
+
+    let infile_name = m.value_of("infile").unwrap();
+
+    let infile = File::open(infile_name)?;
+    let mut buf_reader = BufReader::new(infile);
+    let mut infile_contents = String::new();
+    buf_reader.read_to_string(&mut infile_contents)?;
+
+    let prog = ProgParser::new()
+        .parse(&infile_contents)
+        .map_err(|e| format_err!("{}", e))?;
+
+    println!("{:?}", prog);
+
+    Ok(())
+}
 
 fn main() {
-    println!("hello, ataraxia");
+    if let Err(e) = main_() {
+        e.causes().for_each(|c| eprintln!("{}", c));
+
+        std::process::exit(1);
+    }
 }
